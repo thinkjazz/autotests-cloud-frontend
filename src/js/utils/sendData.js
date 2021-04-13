52d84a01d2ceb73657db18390a40eaa1755a896f
svasenkov <aanher@gmail.com>
2021-04-13 22:41:20 +0300

    some update

--- src/js/utils/sendData.js
+++ src/js/utils/sendData.js
@@ -3,7 +3,7 @@ import "regenerator-runtime/runtime";
 const sendData = async (url, data) => {
   const response = await fetch(url, {
     method: "POST",
-    body: JSON.stringify(data),
+    body: data,
     headers: {
       "Content-Type": "application/json"
     }
