f2c690e9b6266e26f560b02fdfc59fa369e641de
svasenkov <aanher@gmail.com>
2021-04-13 22:33:14 +0300

    some update

--- src/js/utils/sendData.js
+++ src/js/utils/sendData.js
@@ -4,6 +4,9 @@ const sendData = async (url, data) => {
   const response = await fetch(url, {
     method: "POST",
     body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
   });
 
   if (!response.ok) {
