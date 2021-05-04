aca9acbaad5db598343ba48be1876548504287ce
Trouble-Andrew <satyr663@mail.ru>
2021-05-04 17:09:12 +0300

    Меняет положение диско шара

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -3826,7 +3826,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58857" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60037" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
