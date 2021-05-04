ab1455deaf7bedccce4266aaa000a01708cc9592
Trouble-Andrew <satyr663@mail.ru>
2021-05-04 16:55:36 +0300

    Меняет положение дискошара

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -3826,7 +3826,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54196" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58857" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
