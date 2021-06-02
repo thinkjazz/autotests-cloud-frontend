c800eb3448076148f5311f31a671ecdde24f89de
Trouble-Andrew <satyr663@mail.ru>
2021-06-02 17:13:45 +0300

    Меняет прозрачность блока

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -18269,7 +18269,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58641" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60208" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
