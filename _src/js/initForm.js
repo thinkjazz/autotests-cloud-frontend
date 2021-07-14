0ed1c00744f5ca9650e994a6729bf368b5b62b36
heycisco <mail@starchenkov.pro>
2021-07-14 13:23:40 +0300

    +

--- _src/js/initForm.js
+++ _src/js/initForm.js
@@ -3,9 +3,11 @@ import { create_UUID } from "./StringUtils.js";
 const body = document.querySelector("body.tests-app");
 const mainForm = document.querySelector("#objective");
 const mainBtn = document.querySelector("button.main");
+const modalForm = document.querySelector("#modal_form");
 const modalBtn = document.querySelector("button.modal");
 const optionsBtn = document.querySelector("button.options");
 const tcTitle = document.querySelector("#tc_title");
+const tcText = document.querySelector("#text_case");
 const addTestCase = document.querySelector("#add_test_case");
 const tcList = document.querySelector(".added-tc");
 const modalBtnClose = document.querySelector("button.modal-close");
@@ -25,141 +27,141 @@ let scroll;
 
 
 function connect() {
-  console.log('connect +');
-  // const socket = new SockJS("https://api.autotests.cloud/ws"); // todo add exception
-  const socket = new SockJS("http://localhost:8080/ws"); // todo add exception
-  stompClient = Stomp.over(socket);
-  stompClient.connect({}, function(status) {
-    console.log("Connected: " + status);
-    stompClient.subscribe(`/topic/${uuid}`, function(message) {
-      console.log("Message: " + message);
-      // todo add exception
-      addSocketEvent(JSON.parse(message.body));
-    });
-  });
+	console.log('connect +');
+	// const socket = new SockJS("https://api.autotests.cloud/ws"); // todo add exception
+	const socket = new SockJS("http://localhost:8080/ws"); // todo add exception
+	stompClient = Stomp.over(socket);
+	stompClient.connect({}, function(status) {
+		console.log("Connected: " + status);
+		stompClient.subscribe(`/topic/${uuid}`, function(message) {
+			console.log("Message: " + message);
+			// todo add exception
+			addSocketEvent(JSON.parse(message.body));
+		});
+	});
 }
 
 function addSocketEvent(message) {
-  let scrollContent = document.querySelector(".simplebar-content");
-  let pre = document.createElement("pre");
-  pre.setAttribute("data-prefix", message.prefix);
+	let scrollContent = document.querySelector(".simplebar-content");
+	let pre = document.createElement("pre");
+	pre.setAttribute("data-prefix", message.prefix);
 
-  switch (message.contentType) {
-    case "info":
-      pre.className = "list-auto flex";
-      pre.innerHTML = `<code>${message.content}</code>`;
-      break;
-    case "generated":
-      pre.className = "text-warning flex";
-      pre.innerHTML = `<code>${message.content}<a target="_blank" class="green-link" href="${message.url}">${message.urlText}</a></div></code>`;
-      break;
-    case "launched":
-      pre.className = "text-warning flex";
-      pre.innerHTML = `<code>${message.content}<a target="_blank" class="green-link" href="${message.url}">${message.urlText}</a> </code> <label class="loader"></label>`;
-      break;
-    case "finished":
-      pre.className = "text-warning flex";
-      pre.innerHTML = `<code>${message.content}<a target="_blank" class="green-link" href="${message.url}">${message.urlText}</a></div></code>`;
-      document.querySelector(".loader").remove();
-      break;
-    case "in progress":
-      pre.className = "text-warning flex";
-      pre.innerHTML = `<code>${message.content} </code>`;
-      break;
-    case "can-automate":
-      pre.className = "flex";
-      pre.innerHTML = `<code>${message.content}</code>`;
-      break;
-    case "telegram-info":
-      pre.className = "flex";
-      pre.innerHTML = `<code>${message.content}<a target="_blank" class="blue-link" href="${message.url}">${message.urlText}</a> 👈</code>`;
-      break;
-    case "telegram-notification":
-      pre.className = "flex";
-      pre.innerHTML = `<code> </code>`;
-      displayNotification(message.content);
-      break;
-    case "empty":
-      pre.className = "list-auto flex";
-      pre.innerHTML = `<code> </code>`;
-      break;
-    case "error":
-      pre.className = "text-error flex";
-      pre.innerHTML = `<code>${message.content}</code>`;
-      break;
-  }
-  scrollContent.append(pre);
-  scroll.getScrollElement().scrollTo({ top: 5000, behavior: "smooth" });
+	switch (message.contentType) {
+		case "info":
+			pre.className = "list-auto flex";
+			pre.innerHTML = `<code>${message.content}</code>`;
+			break;
+		case "generated":
+			pre.className = "text-warning flex";
+			pre.innerHTML = `<code>${message.content}<a target="_blank" class="green-link" href="${message.url}">${message.urlText}</a></div></code>`;
+			break;
+		case "launched":
+			pre.className = "text-warning flex";
+			pre.innerHTML = `<code>${message.content}<a target="_blank" class="green-link" href="${message.url}">${message.urlText}</a> </code> <label class="loader"></label>`;
+			break;
+		case "finished":
+			pre.className = "text-warning flex";
+			pre.innerHTML = `<code>${message.content}<a target="_blank" class="green-link" href="${message.url}">${message.urlText}</a></div></code>`;
+			document.querySelector(".loader").remove();
+			break;
+		case "in progress":
+			pre.className = "text-warning flex";
+			pre.innerHTML = `<code>${message.content} </code>`;
+			break;
+		case "can-automate":
+			pre.className = "flex";
+			pre.innerHTML = `<code>${message.content}</code>`;
+			break;
+		case "telegram-info":
+			pre.className = "flex";
+			pre.innerHTML = `<code>${message.content}<a target="_blank" class="blue-link" href="${message.url}">${message.urlText}</a> 👈</code>`;
+			break;
+		case "telegram-notification":
+			pre.className = "flex";
+			pre.innerHTML = `<code> </code>`;
+			displayNotification(message.content);
+			break;
+		case "empty":
+			pre.className = "list-auto flex";
+			pre.innerHTML = `<code> </code>`;
+			break;
+		case "error":
+			pre.className = "text-error flex";
+			pre.innerHTML = `<code>${message.content}</code>`;
+			break;
+	}
+	scrollContent.append(pre);
+	scroll.getScrollElement().scrollTo({ top: 5000, behavior: "smooth" });
 }
 
 function displayNotification(messagePath) {
-  iframeBlock.innerHTML = `<iframe id="telegram-post-autotests_cloud-17" class="telegram-iframe w-full h-full h-80"
-          src="https://t.me/${messagePath}?embed=1&discussion=1&comments_limit=5&dark=1"></iframe>`;
+	iframeBlock.innerHTML = `<iframe id="telegram-post-autotests_cloud-17" class="telegram-iframe w-full h-full h-80"
+					src="https://t.me/${messagePath}?embed=1&discussion=1&comments_limit=5&dark=1"></iframe>`;
 }
 
 const initForm = () => {
-  connect();
+	connect();
 
-  function submitForm(event) {
-    event.preventDefault();
-    alert(document.getElementById('input-title').value);
+	function submitForm(event) {
+		event.preventDefault();
+		alert(document.getElementById('input-title').value);
 
-    const formData = new FormData(mainForm);
-    const values = Object.fromEntries(formData.entries());
+		const formData = new FormData(mainForm);
+		const values = Object.fromEntries(formData.entries());
 
-    console.log(values);
+		console.log(values);
 
-    if (values.title) {
-      values.price = "free";
-      values.email = "admin@qa.guru";
-      values.steps = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-      // values.captcha = values["g-recaptcha-response"];
-      delete values["g-recaptcha-response"];
+		if (values.title) {
+			values.price = "free";
+			values.email = "admin@qa.guru";
+			values.steps = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+			// values.captcha = values["g-recaptcha-response"];
+			delete values["g-recaptcha-response"];
 
-      console.log(values);
+			console.log(values);
 
-      stompClient.send(`/app/orders/${uuid}`, {}, JSON.stringify(values));
+			stompClient.send(`/app/orders/${uuid}`, {}, JSON.stringify(values));
 
-      consoleContainer.classList.remove("hidden");
-      mainForm.classList.add("hidden");
-      iframeBlock.classList.remove("hidden");
-      infoBlock.classList.add("hidden");
+			consoleContainer.classList.remove("hidden");
+			mainForm.classList.add("hidden");
+			iframeBlock.classList.remove("hidden");
+			infoBlock.classList.add("hidden");
 
-      scroll = new SimpleBar(consoleWindow, { autoHide: false });
+			scroll = new SimpleBar(consoleWindow, { autoHide: false });
 
-      function add() {
-        let pre = document.createElement("pre");
-        let scrollContent = document.querySelector(".simplebar-content");
-        pre.setAttribute("data-prefix", "$");
+			function add() {
+				let pre = document.createElement("pre");
+				let scrollContent = document.querySelector(".simplebar-content");
+				pre.setAttribute("data-prefix", "$");
 
-        pre.innerHTML = `<code>npm i daisyui</code>`;
+				pre.innerHTML = `<code>npm i daisyui</code>`;
 
-        scrollContent.append(pre);
+				scrollContent.append(pre);
 
-        scroll.getScrollElement().scrollTo({ top: 5000, behavior: "smooth" });
-        // scroll.getScrollElement().scrollTop = scroll.getScrollElement().scrollHeight;
-      }
+				scroll.getScrollElement().scrollTo({ top: 5000, behavior: "smooth" });
+				// scroll.getScrollElement().scrollTop = scroll.getScrollElement().scrollHeight;
+			}
 
-      // window.setInterval(add, 2500);
+			// window.setInterval(add, 2500);
 
-      mainForm.reset();
-    } else {
-      // if (!mainTextarea.value) {
-      //   mainTextarea.classList.add("border-red-500");
-      // }
+			mainForm.reset();
+		} else {
+			// if (!mainTextarea.value) {
+			//   mainTextarea.classList.add("border-red-500");
+			// }
 
-      if (!titleTextarea.value) {
-        titleTextarea.classList.add("border-red-500");
-      }
+			if (!titleTextarea.value) {
+				titleTextarea.classList.add("border-red-500");
+			}
 
-      setTimeout(() => {
-        // mainTextarea.classList.remove("border-red-500");
-        titleTextarea.classList.remove("border-red-500");
-      }, 2000);
-    }
-  }
+			setTimeout(() => {
+				// mainTextarea.classList.remove("border-red-500");
+				titleTextarea.classList.remove("border-red-500");
+			}, 2000);
+		}
+	}
 
-  mainBtn.addEventListener("click", submitForm);
+	mainBtn.addEventListener("click", submitForm);
 };
 
 export { initForm };
@@ -167,25 +169,61 @@ export { initForm };
 initForm();
 
 function modalOpen() {
- body.classList.add("modal");
+	body.classList.add("modal");
 }
-function modalClose() {
- body.classList.remove("modal");
-}
-function optionsToggle() {
- body.classList.toggle("options-open");
-}
-function addNewField() {
-  formAdd.insertAdjacentHTML("beforeend", `<div class="input-block"><input type="text" id="tc_title_` + 1 + `" placeholder="Lorem ipsum dolor sit">
-        <textarea name="test-case" id="text_case" rows="8" placeholder="Lorem ipsum dolor sit amet,
-consectetur adipisicing elit, sed do eiusmod
 
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat"></textarea></div>`);
+function modalClose() {
+	body.classList.remove("modal");
 }
+
+function optionsToggle() {
+	body.classList.toggle("options-open");
+}
+
+// localStorage.clear();
+
+var tcTitles = [];
+var tcTexts = [];
+
+
 function createTestCase() {
-  tcList.insertAdjacentHTML("beforeend", `<li>` + tcTitle.value + `</li>`);
+
+	if (!tcTitle.value) {
+		tcTitle.classList.add("border-red-500");
+	}
+
+	setTimeout(() => {
+		tcTitle.classList.remove("border-red-500");
+	}, 2000);
+	if (tcTitle.value) {
+	tcList.insertAdjacentHTML("beforeend", `<li>` + tcTitle.value + `</li>`);
+		tcTitles.push(tcTitle.value);
+		tcTexts.push(tcText.value);
+		localStorage.setItem('title', tcTitles);
+		localStorage.setItem('text', tcTexts);
+		modalForm.reset();
+		body.classList.remove("modal");
+	}
+}
+
+
+
+function addNewField() {
+
+	if (!tcTitle.value) {
+		tcTitle.classList.add("border-red-500");
+	}
+	setTimeout(() => {
+		tcTitle.classList.remove("border-red-500");
+	}, 2000);
+	if (tcTitle.value) {
+		tcList.insertAdjacentHTML("beforeend", `<li>` + tcTitle.value + `</li>`);
+		tcTitles.push(tcTitle.value);
+		tcTexts.push(tcText.value);
+		localStorage.setItem('title', tcTitles);
+		localStorage.setItem('text', tcTexts);
+		modalForm.reset();
+	}
 }
 
 
@@ -195,4 +233,3 @@ modalBtnClose.addEventListener("click", modalClose);
 optionsBtn.addEventListener("click", optionsToggle);
 addNew.addEventListener("click", addNewField);
 addTestCase.addEventListener("click", createTestCase);
-
