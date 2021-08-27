643087808609d95d9a2b6ce87699fd907d7a92c7
Bragin Boris <bragin.boris@ya.ru>
2021-08-27 12:38:04 +0300

    Add sanitazer module

---
+++ _src/js/htmlSanitaizer.js
@@ -0,0 +1,25 @@
+let _onPaste_StripFormatting_IEPaste = false;
+
+function OnPaste_StripFormatting(elem, e) {
+
+    if (e.originalEvent && e.originalEvent.clipboardData && e.originalEvent.clipboardData.getData) {
+        e.preventDefault();
+        let text = e.originalEvent.clipboardData.getData('text/plain');
+        window.document.execCommand('insertText', false, text);
+    }
+    else if (e.clipboardData && e.clipboardData.getData) {
+        e.preventDefault();
+        let text = e.clipboardData.getData('text/plain');
+        window.document.execCommand('insertText', false, text);
+    }
+    else if (window.clipboardData && window.clipboardData.getData) {
+        // Stop stack overflow
+        if (!_onPaste_StripFormatting_IEPaste) {
+            _onPaste_StripFormatting_IEPaste = true;
+            e.preventDefault();
+            window.document.execCommand('ms-pasteTextOnly', false);
+        }
+        _onPaste_StripFormatting_IEPaste = false;
+    }
+
+}
